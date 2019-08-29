c4a3d525b8f737d92e681c823cf02d977f5eb5fd
dingdingLee <blej25@gmail.com>
2019-08-29 12:41:47 +0900

    회원상세정보(작성자정보)보기

--- PorGet/src/main/java/com/porget/control/PortfolioController.java
+++ PorGet/src/main/java/com/porget/control/PortfolioController.java
@@ -25,7 +25,6 @@ public class PortfolioController {
 	@RequestMapping("/partPopular")
 	public String showPopularPart(Model m, int base) { //인기포트폴리오 게시판 스크롤 내려가면 그 다음 목록 검색해서 뿌려주기
 		List<Map<String, Object>> list = dao.showPopularPart(base);
-		System.out.println(list);
 		m.addAttribute("list", list);
 		return "portfolio/popularBoardPart";
 	}
